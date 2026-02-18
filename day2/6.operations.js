64a5380522f294400f107162dc033747a96138b7
madiravijay402 <vijaymadira681@gmail.com>
2026-02-18 22:31:07 +0530

    6,7,8 commit

---
+++ day2/6.operations.js
@@ -0,0 +1,21 @@
+let num1=40;
+let num2=3;
+
+// addition operator
+console.log(num1+num2);
+
+// subtraction operator 
+
+console.log(num1-num2);
+
+// multiplication operator
+
+console.log(num1*num2);
+
+// divison operator
+
+console.log((num1/num2));
+
+// modulus operator
+
+console.log(num1%num2);
---
+++ day2/7.declareVairable.js
@@ -0,0 +1,14 @@
+let num=30;
+
+num+=10;
+console.log(num);
+
+num-=10;
+console.log(num);
+
+num*=12;
+console.log(num);
+
+num/=12;
+console.log(num);
+
