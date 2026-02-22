d307d979c8fede857970232975aa183b26cabeea
madiravijay402 <vijaymadira681@gmail.com>
2026-02-22 12:10:52 +0530

    commit 9

---
+++ day3/9.Object with Array.js
@@ -0,0 +1,10 @@
+const product = {
+  name: "Laptop",
+  features: ["16GB RAM", "512GB SSD", "i7 Processor"]
+};
+// Second feature
+console.log("Second features" ,product.features[1]);
+
+// Total number of features
+console.log("total No of features : ",product.features.length);
+
