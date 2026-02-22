f2e2a5fe71de09c8b56f3a7aa7d49a6f1c4433fa
madiravijay402 <vijaymadira681@gmail.com>
2026-02-22 11:59:45 +0530

    7 commit

---
+++ day3/7.modifyProperty.js
@@ -0,0 +1,15 @@
+const car = {
+  brand: "BMW",
+  year: 2020
+};
+
+// update year to 2024;
+console.log(car);
+car.year=2024;
+console.log(car);
+
+//Add new property: color = "Black"
+
+car.color="black";
+console.log(car);
+
