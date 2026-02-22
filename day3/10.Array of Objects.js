045a4e1f2f767f766ba8f57130e0a26eb482b6e3
madiravijay402 <vijaymadira681@gmail.com>
2026-02-22 12:15:00 +0530

    10 commit

---
+++ day3/10.Array of Objects.js
@@ -0,0 +1,10 @@
+const students = [
+  { name: "Arun", marks: 80 },
+  { name: "Ravi", marks: 40 },
+  { name: "Kumar", marks: 90 }
+];
+for(let i=0;i<students.length;i++){
+    if(students[i].marks>50){
+        console.log(students[i].name,students[i].marks);
+    }
+}
