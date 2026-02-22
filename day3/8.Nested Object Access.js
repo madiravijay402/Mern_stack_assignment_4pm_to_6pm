dfa9b26db6905121b1e5bfd1eb598134ea5d7ba3
madiravijay402 <vijaymadira681@gmail.com>
2026-02-22 12:03:00 +0530

    8 commit

---
+++ day3/8.Nested Object Access.js
@@ -0,0 +1,15 @@
+const employee = {
+  name: "Ravi",
+  address: {
+    city: "Chennai",
+    pincode: 600078
+  }
+};
+
+//city
+
+console.log(employee.address.city);
+
+// pincode
+
+console.log(employee.address.pincode);
