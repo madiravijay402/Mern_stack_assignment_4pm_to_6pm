caf8cbc7a05d49372c5be916ac3d6d5353af6bf6
madiravijay402 <vijaymadira681@gmail.com>
2026-02-23 21:48:45 +0530

    1 commit

---
+++ day4/1.Implicit.js
@@ -0,0 +1,11 @@
+// Implicit 
+
+let a=20+"20"; // it will convert 20  umber into string and concatenate it with "20" and give 2020 as output
+console.log(a); 
+
+let b=20-"5";// string will be converted into number
+console.log(b);
+
+console.log("output:",[]+{});// when we add array and object it will convert both into string and concatenate them and give [object Object] as output
+
+console.log("output:",[]+[]);
