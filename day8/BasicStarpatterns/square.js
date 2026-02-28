41d4b9bc3466e761c7b592f8d35e7814c79cd976
madiravijay402 <vijaymadira681@gmail.com>
2026-02-28 19:12:32 +0530

    pattern

---
+++ day8/BasicStarpatterns/square.js
@@ -0,0 +1,9 @@
+let a=5;
+for(let i=1;i<=a;i++){
+    let str="";
+    for(let j=1;j<=a;j++){
+        str+="*"+" ";
+    }
+    console.log(str);
+    
+}
--- forLooppractice/text.js
+++ forLooppractice/text.js
@@ -1,5 +1,12 @@
-for(let i=0;i<10;i++){
-    console.log(i);
-    
+let a=[12,3,4,5,6];
+let reuslt=[];
+sum=0;
+for(let i=0;i<a.length;i++){
+    reuslt+=a[i]+" ";
+    sum+=a[i];
 }
+console.log(reuslt);
+console.log(sum);
+
+
 
